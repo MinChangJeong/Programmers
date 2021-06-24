f4ae18a3ddc76392b35e72dd0b52e3dcdd1d7985
origin <wjdalsckd777@naver.com>
2021-06-24 16:26:12 +0900

    프로그래머스_전화번호목록

--- 프로그래머스_전화번호목록/Programmers4.java
+++ 프로그래머스_전화번호목록/Programmers4.java
@@ -1,17 +1,35 @@
 package 프로그래머스_전화번호목록;
 
+import java.util.*;
 // 이 코드의 문제 hash로 구현하지 못해 시간초과 에러가 남..
+// 알고리즘 문제에서 "노가다 문자열 비교 작업"이 필요하다면 해시를 먼저 떠올리면 좋다.
 public class Programmers4 {
 	public boolean solution(String[] phone_book) {
         boolean answer = true;
-        for(String phone : phone_book) {
-        	for(String target : phone_book) {
-        		String [] num = target.split(phone);
-        		if(num.length == 2 && num[0].length()==0) {
+        Arrays.sort(phone_book);
+        
+        for(int i=0; i<phone_book.length; i++) {
+        	for(int j=i+1; j<phone_book.length; j++) {
+        		if(phone_book[i] == phone_book[j])
+        			continue;
+        		if(phone_book[j].indexOf(phone_book[i]) == 0) {
+        			answer = false;
         			return false;
         		}
         	}
         }
+//        풀이 2
+//        for(String phone : phone_book) {
+//        	for(String target : phone_book) {
+//        		if(phone.length() <= target.length()) {
+//        			if(!target.equals(phone) && target.contains(phone) && target.indexOf(phone) == 0) {
+//        				answer = false;
+//        				break;
+//        			}
+//        		}
+//        	}
+//        }
+        
         return answer;
     }
 }
--- 프로그래머스_전화번호목록/Programmers4P.java
+++ 프로그래머스_전화번호목록/Programmers4P.java
@@ -1,5 +1,17 @@
 package 프로그래머스_전화번호목록;
 
+import java.util.*;
 public class Programmers4P {
-
+	public boolean solution(String[] phone_book) {
+		boolean answer = true;
+		
+		
+        return answer;
+	}
+	
+	public static void main(String []args) {
+		Programmers4P p = new Programmers4P();
+		String [] phone_book = {"44","4544"};
+		p.solution(phone_book);
+	}
 }
