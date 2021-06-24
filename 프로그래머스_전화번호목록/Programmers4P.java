36540d611e1daa16afe2311a6bd908287c903e44
origin <wjdalsckd777@naver.com>
2021-06-24 16:33:35 +0900

    프로그래머스_전화번호목록

--- 프로그래머스_전화번호목록/Programmers4P.java
+++ 프로그래머스_전화번호목록/Programmers4P.java
@@ -5,7 +5,20 @@ public class Programmers4P {
 	public boolean solution(String[] phone_book) {
 		boolean answer = true;
 		
+		HashMap <String, String> hashmap = new HashMap<String, String>();
 		
+		for(String phone : phone_book) {
+			hashmap.put(phone, "prefix");
+		}
+		
+		for(String phone : phone_book) {
+			for(int i=0; i<phone.length(); i++) {
+				if(hashmap.containsKey(phone.substring(0, i))) {
+					answer = false;
+					return answer;
+				}
+			}
+		}
         return answer;
 	}
 	
