24359d497e24599151adecdf8f5493b48b7f358b
origin <wjdalsckd777@naver.com>
2021-06-24 17:45:24 +0900

    프로그래머스

---
+++ 프로그래머스_베스트앨범/Programmers8.java
@@ -0,0 +1,10 @@
+package 프로그래머스_베스트앨범;
+
+import java.util.*;
+
+public class Programmers8 {
+	public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+}
