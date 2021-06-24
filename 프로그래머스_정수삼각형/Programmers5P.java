09281a034e4e90234f36ea1b312fb7d42ff4b545
origin <wjdalsckd777@naver.com>
2021-06-24 09:47:12 +0900

    프로그래머스_정수삼각형

--- 프로그래머스_정수삼각형/Programmers5P.java
+++ 프로그래머스_정수삼각형/Programmers5P.java
@@ -1,14 +1,46 @@
 package 프로그래머스_정수삼각형;
 
+// 동적계획법 (memoization활용)
 public class Programmers5P {
-	public int solution(int[][] triangle) {        
-        // 이문제에서 caching은 bottom-up으로 생각했을때 해당 삼각형의 계산값의 최대값은 항상 같음
-		return 0;
-	}
-
-	public static void main(String[] args) {
-		Programmers5P p = new Programmers5P();
-		int [][] triangle = {{7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5}};
-		System.out.println(p.solution(triangle));
+	public int solution(int[][] triangle) {   
+		int answer = 0;
+				
+		int [][] memoization = new int [triangle.length][triangle.length];
+		
+       for(int i=0; i<triangle.length; i++) {
+    	   for(int j=0; j<=i; j++) {
+    		   memoization[i][j] = -1;
+    	  }
+       } 
+       
+       for(int layer=0; layer<triangle.length; layer++) {
+    	   for(int idx=0; idx<=layer; idx++) {
+    		   if(layer == 0) {
+    			   memoization[layer][idx] = triangle[layer][idx];
+    		   }
+    		   else if(idx == layer) {
+    			   memoization[layer][idx] = triangle[layer][idx] + memoization[--layer][--idx];
+    			   layer+=1;
+    			   idx+=1;
+    		   }
+    		   else if(idx == 0) {
+    			   memoization[layer][idx] = triangle[layer][idx] + memoization[--layer][idx];
+    			   layer+=1;
+    		   }
+    		   else {
+    			   int node = triangle[layer][idx];
+				   memoization[layer][idx] = Math.max(node + memoization[--layer][idx], node+memoization[layer][--idx]);
+				   layer+=1;
+				   idx+=1;
+						   
+    		   }
+    	   }
+       }
+       
+       for(int i = 0; i<memoization[memoization.length-1].length; i++) {
+    	   if(answer<=memoization[memoization.length-1][i])
+    		   answer = memoization[memoization.length-1][i];
+       }
+       return answer;
 	}
 }
