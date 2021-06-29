e64f2b18d79e17b6f88d2e99e74d46fd74b02872
origin <wjdalsckd777@naver.com>
2021-06-29 12:27:06 +0900

    프로그래머스_구명보트

--- 프로그래머스_구명보트/Programmers9.java
+++ 프로그래머스_구명보트/Programmers9.java
@@ -1,7 +1,5 @@
 package 프로그래머스_구명보트;
 
-// 시간초과 에러가 남..
-// Greedy방식으로 푸는건 뭔가에 대한 고민!!
 public class Programmers9 {
 	public int solution(int[] people, int limit) {
         int answer = 0;
@@ -11,6 +9,7 @@ public class Programmers9 {
         int idx = 0;
         int t_idx = 0;
         
+        int minus = 0;
         for(int i=0; i<people.length; i++) {
         	int sum=0;
         	for(int j=0; j<people.length; j++) {
@@ -26,15 +25,13 @@ public class Programmers9 {
             	check[idx] = true;
             	check[t_idx] = true;
         	}
+        	if(check[i]) {
+        		minus+=1;
+        	}
         	answer+=1;
         }
-        int sum = 0;
-        for(boolean c : check) {
-        	if(c)
-        		sum+=1;
-        }
-        answer-=sum/2;
+        answer-=minus/2;
         return answer;
     }
-	
+
 }
--- 프로그래머스_구명보트/Programmers9P.java
+++ 프로그래머스_구명보트/Programmers9P.java
@@ -1,5 +1,21 @@
 package 프로그래머스_구명보트;
 
-public class Programmers9P {
+import java.util.*;
 
+public class Programmers9P {
+	public int solution(int[] people, int limit) {
+        int rightPoint = people.length-1;
+        int result = 0;
+        Arrays.sort(people);
+        
+        for(int i = 0; i <= rightPoint; i++) {
+            while(i < rightPoint && people[i] + people[rightPoint--] > limit) {
+                result++;
+            }
+            result++;
+        }
+        
+        return result;
+        
+    }
 }
