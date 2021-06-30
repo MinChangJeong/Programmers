ca2857c5ada070193c382f259d9a3420252e1e91
origin <wjdalsckd777@naver.com>
2021-06-30 15:33:53 +0900

    프로그래머스_디스크컨트롤러

---
+++ 프로그래머스_디스크컨트롤러/Programmers10.java
@@ -0,0 +1,17 @@
+package 프로그래머스_디스크컨트롤러;
+
+import java.util.*;
+
+public class Programmers10 {
+	public int solution() {
+        int answer = 0;
+
+        
+        return answer;
+    }
+	
+	public static void main(String [] args) {
+		Programmers10 p = new Programmers10();
+		p.solution();
+	}
+}
