d0f3aea6b954d35f9d5369e2202194c2c16d6966
origin <wjdalsckd777@naver.com>
2021-07-16 11:16:03 +0900

    Dynamic Programming Ex

--- Dynamic_Programming/DP3.java
+++ Dynamic_Programming/DP3.java
@@ -3,7 +3,42 @@ package Dynamic_Programming;
 // Table
 public class DP3 {
 	public static void main(String [] args) {
+		int answer=0;
+		
+		int n = 100;
+		int node = 10;
+		int	[] dp = new int[n-1];
+		
+		
+		answer = dynamic(answer, node, n, dp);
+		System.out.println(answer);
+		
+	}
+	
+	// 완전 탐색이 되어버림..
+	static int dynamic(int answer, int node, int n, int [] dp) {
+		if(n == 0) {
+			answer +=1;
+			return answer;
+		}
+		if(n == 1) {
+			return answer;
+		}
+		
+		for(int i=node; i>=2; i--) {
+			if(n<i) {
+				continue;
+			}
+			answer=dynamic(answer, i, n-i, dp);
 
-	}	
+		}
+		
+		return answer;
+	}
+	
+	// 동적계획법으로 다시 풀자
+	static int dynamicP() {
+		
+	}
 }
 
