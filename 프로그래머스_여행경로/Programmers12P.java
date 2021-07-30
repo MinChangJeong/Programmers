e287578ff5ead7b0c16357ee634fb5276f6293dd
origin <wjdalsckd777@naver.com>
2021-07-30 09:42:26 +0900

    프로그래머스_여행경로

--- 프로그래머스_여행경로/Programmers12P.java
+++ 프로그래머스_여행경로/Programmers12P.java
@@ -3,9 +3,7 @@ package 프로그래머스_여행경로;
 import java.util.*;
 
 public class Programmers12P {
-    public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        
+    public String[] solution(String[][] tickets) {    
         Arrays.sort(tickets, new Comparator<String[]>() {
 			
         public int compare(String[] o1, String[] o2) {
@@ -15,11 +13,43 @@ public class Programmers12P {
                 return o1[0].toString().compareTo(o2[0].toString());
         	}			
     	});
+    
+        Stack<String> stack = new Stack<String>();
+        stack.add("ICN");
+        
+        List<String> result = new ArrayList<String>();
+        
+        boolean [] check = new boolean[tickets.length];
+        
+        dfs(tickets, stack, result, check);
+        
+        String[] answer = new String[result.size()];
         
         
-        
+        for(int idx=result.size()-1; idx>=0; idx--) {
+        	answer[answer.length-idx-1] = result.get(idx);
+        }
         
         return answer;
+    }
+    
+    void dfs(String[][] tickets, Stack<String> stack, List<String> result, boolean [] check) {
+    	if(stack.size()!=0) {
+    		boolean stack_change_check = false;
+    		for(int idx=0; idx<tickets.length; idx++) {
+    			if(!check[idx] && tickets[idx][0].equals(stack.peek())) {
+    				stack.add(tickets[idx][1]);
+    				check[idx] = true;
+    				stack_change_check = true;
+    				dfs(tickets, stack, result, check);
+    			}
+    		}
+    		if(!stack_change_check) {
+    			result.add(stack.pop());
+    			dfs(tickets, stack, result, check);
+    		}
+    		
+    	}
     }
 
        
